cf1f09189a23ea18875f7a7056c45404a015561b
hmnsh17 <hmnshgrvr@gmail.com>
2022-12-21 15:05:19 +0530

    Client side validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   validates :name,presence: true,length: { maximum: 10 }
   validates :title,presence: true,length: { maximum: 10 }
-  validates :description,presence: true,length: { maximum: 10 }
+  validates :description,presence: true,length: { maximum: 255 }
 end
